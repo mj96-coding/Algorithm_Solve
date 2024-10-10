b413b7e1ec98deda0373004e5ae3a179f2bb7888
mj96-coding <mjbori@naver.com>
2024-10-10 20:26:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.65 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,35 +1,12 @@
 #include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-
-
 int solution(vector<int> nums)
 {
-    int answer = 0;
+    unordered_set<int> ans(nums.begin(),nums.end());
     
-    int dat[200001]={};    
-    
-    int cnt=0;
-    
-    for(int i : nums)
-    {
-        if(dat[i]==0)
-        {
-            dat[i]=1;
-            cnt++;
-        }
-    }
-    
-    if(cnt >= nums.size()/2)
-    {
-        answer=nums.size()/2;
-    }
-    else
-    {
-        answer=cnt;
-    }
-    
-    
-    return answer;
+    return min(nums.size()/2,ans.size());
 }
